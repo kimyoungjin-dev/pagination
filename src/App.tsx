e2a8fdda05acac31f13fb62ca9198abdfe3f89a7
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-08 22:51:31 +0900

    Update App.tsx
    Get data from getData:axios.
    Create lastPost, firstPost, and currentPost variables.
    
    It delivers loading and data information to Posts.tsx.
    Passes postPerPage, totalPosts, and paginateFn to Pagination.tsx.
    
    It takes paginateFn:value as an argument and moves to the page that matches number.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { paginationApi } from "./api";
+import Pagination from "./Pagination";
+import Posts from "./Posts";
+import GlobalStyle from "./GlobalStyle";
 
-function App() {
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState([]);
+  const [currentPages, setCurrentPages] = useState(1); //현재페이지수
+  const [postPerPages] = useState(10); //한화면에 랜더링할 정보의 갯수
+
+  const getData = async () => {
+    try {
+      const { data } = await paginationApi();
+      setResponse(data);
+    } catch (error) {
+      console.log(error);
+      return error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const lastPost = currentPages * postPerPages; //10
+  const firstPost = lastPost - postPerPages; //0
+  const currentPost = response.slice(firstPost, lastPost); //(0~10)
+
+  const paginateFn = (value: number) => setCurrentPages(value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <Posts loading={loading} response={currentPost} />
+      <Pagination
+        postPerPage={postPerPages}
+        totalPosts={response.length}
+        paginateFn={paginateFn}
+      />
     </div>
   );
 }
-
-export default App;
